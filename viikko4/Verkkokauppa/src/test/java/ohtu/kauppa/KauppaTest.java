ffbbb1b8af1a31d10fb8cd3ed4e69ffa0b28739f
Riina-Alisa Huotari <riina-alisa.huotari@helsinki.fi>
2019-11-21 13:14:20 +0200

    Viikko 4, tehtävä 4

--- viikko4/Verkkokauppa/src/test/java/ohtu/kauppa/KauppaTest.java
+++ viikko4/Verkkokauppa/src/test/java/ohtu/kauppa/KauppaTest.java
@@ -75,7 +75,49 @@ public class KauppaTest {
         kauppa.tilimaksu("pekka", "12345");
 
         verify(pankki).tilisiirto(eq("pekka"), anyInt(), eq("12345"), eq("33333-44455"), eq(5));
+    }
 
+    @Test
+    public void metodinAloitaAsiointiKutsuminenNollaaEdellisenOstoksenTiedot() {
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);
+        kauppa.tilimaksu("pekka", "12345");
+
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);
+        kauppa.tilimaksu("pekka", "12345");
+
+        verify(pankki, times(2)).tilisiirto(anyString(), anyInt(), anyString(), anyString(), eq(5));
+    }
+
+    @Test
+    public void jokaiselleMaksutapahtumallePyydetaanUusiViitenumero() {
+        when(viite.uusi()).thenReturn(40);
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);
+        kauppa.tilimaksu("pekka", "12345");
+        verify(pankki, times(1)).tilisiirto(anyString(), eq(40), anyString(), anyString(), anyInt());
+
+        when(viite.uusi()).thenReturn(41);
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);
+        kauppa.tilimaksu("pekka", "12345");
+
+        verify(pankki, times(1)).tilisiirto(anyString(), eq(41), anyString(), anyString(), anyInt());
+    }
+
+    @Test
+    public void ostoskoriinLisattyTuotePoistetaanKorista() {
+        when(varasto.saldo(2)).thenReturn(10);
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "leipä", 3));
+
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);
+        kauppa.lisaaKoriin(2);
+        kauppa.poistaKorista(1);
+        kauppa.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(), eq(3));
     }
 
 }
